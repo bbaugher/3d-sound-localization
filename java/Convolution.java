35093191aea5d7d5333a92fe37b0ad990e878096
bjbq4d@gmail.com <bjbq4d@gmail.com@16028747-3dae-4f48-4921-cec73a656796>
2011-02-09 15:02:27 +0000

    Added all the files so everything is up to date.

--- java/Convolution.java
+++ java/Convolution.java
@@ -7,6 +7,8 @@
 import java.lang.*;
 import java.io.*;
 import javax.sound.sampled.*;
+//import edu.emory.mathcs.jtransforms.fft;
+//import edu.emory.mathcs.utils.IOUtils;
 
 class Convolution {
 
@@ -31,48 +33,6 @@ class Convolution {
 		}
 		File outfile = new File(outfileName);
 
-		/*
-		//Original WavReader Version
-
-		WavReader wr = new WavReader(new File(infilename));
-
-		// load the entire sound into memory
-
-		int theSize = wr.getNumSamples();
-
-		short[] theSound = wr.getShortSamples(); // new int[theSize];
-
-		// result, also in memory, actual space allocated by convolve
-		short [] theResult;
-
-		// files are now open/created/loaded
-
-		int samplerate = wr.getSampleRate();
-		System.err.println("Sample rate: " + samplerate + ", sample size: " +
-				wr.getSampleSize());
-
-		double lowerFrequency = 600; // in Hertz
-		double upperFrequency = 1200;
-		System.err.println("Cutoff frequencies: lower=" + lowerFrequency+ " upper=" +
-			upperFrequency);
-
-		//======================================================================
-
-		// first kernel
-
-		int len1 = 1000;	// random value
-		double[] k1 = new double[len1];
-
-		// now initialize k1
-
-		// here we do the convolution operation; may need twice for bandpass!
-		theResult = convolve(theSound, k1);
-
-		// now output the result
-		WavReader.writeWav(theResult, samplerate, outfile);
-
-		*/
-
 		//WavFile Version
 		try
 		{
@@ -90,6 +50,10 @@ class Convolution {
 			
 			w1.readFrames(w1_buffer, (int)w1_numFrames);
 			
+			//System.out.println("Length = "+w1_numFrames);
+			//DoubleFFT_1D w1_ft = new DoubleFFT_1D((int)w1_numFrames);
+			//w1_ft.realForwardFull(w1_buffer[0]);
+
 			// Open the wav file specified as the second argument
 			WavFile w2 = WavFile.openWavFile(new File(w2_filename));
 			
@@ -119,40 +83,6 @@ class Convolution {
 			newSound[1] = convolve_double(w2_buffer[1], w1_buffer[0]);
 			System.out.println("Right Channel Finished");
 
-			/*
-
-			//An attempt at using FFT convolution
-
-			Complex[][] w1_complex = new Complex[(int)w1_numChannels][(int)w1_numFrames]; 
-			for(int i=0; i<(int)w1_numChannels; i++){
-				for(int j=0; j<(int)w1_numFrames; j++){
-					w1_complex[i][j] = new Complex(w1_buffer[i][j], 0);
-				}
-			}
-
-			Complex[][] w2_complex = new Complex[(int)w2_numChannels][(int)w2_numFrames];
-                        for(int i=0; i<(int)w2_numChannels; i++){
-                                for(int j=0; j<(int)w2_numFrames; j++){
-                                        w2_complex[i][j] = new Complex(w2_buffer[i][j], 0);
-                                }
-                        }
-
-			cstart = System.currentTimeMillis();
-
-			Complex[][] final_complex = new Complex[2][new_frames];
-			final_complex[0] = FFT.convolve(w1_complex[0], w2_complex[0]);
-			final_complex[1] = FFT.convolve(w1_complex[0], w2_complex[1]);
-			
-			for(int i=0; i<2; i++){
-				for(int j=0; j<new_frames; j++){
-					newSound[i][j] = final_complex[i][j].re();
-				}
-			}			
-
-			cend = System.currentTimeMillis();
-
-			*/
-
 			out.writeFrames(newSound, new_frames);
 			System.out.println("Sound File Written.");
 
@@ -160,35 +90,12 @@ class Convolution {
 			total = end - start;
 
 			System.out.println("Process took "+total+" milliseconds.");
-			//System.out.println("Convolution took "+(cend-cstart)+" milliseconds.");
 
 		}
 		catch(Exception e){
 				System.err.println(e);
 		}
 		
-	}
-
-	/*
-	 * I'm giving this to you, as the implementation is rather messy.
-	 * This is the version with full "tails" implemented: a value is assigned
-	 * whenever samples and the kernel have any overlap at all.
-	 * As of June 19, it is only partially tested.
-	 */
-	static public short[] convolve(short[] samples, double[] kernel) {
-		int len = kernel.length;
-		short[] result = new short[samples.length + len-1];
-		for (int n = 0; n < result.length; n++) {
-			double sum = 0;
-			for (int i = 0; i<len; i++) {
-				if (i<samples.length + (len-1) - n && i >= len-n-1) {
-					sum += samples[i+n-len+1]*kernel[len-i-1];
-				}
-			}
-			result[n] = (short) sum;
-
-		}
-		return result;
 	}
 
 	static public double[] convolve_double(double[] HRTF, double[] source){
