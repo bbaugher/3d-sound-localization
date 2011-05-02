6e03cec77c031a41b3c975cf5473d30ffc19daba
bjbq4d@gmail.com <bjbq4d@gmail.com@16028747-3dae-4f48-4921-cec73a656796>
2011-05-02 01:23:51 +0000

    Updated Create_Customization_Files. Cleaned up the code and added an option to give a volume

--- java/Create_Customization_Files.java
+++ java/Create_Customization_Files.java
@@ -10,37 +10,20 @@ class Create_Customization_Files {
 
     public static void main(String[] args) {
 		if (args.length != 5) {
-			System.err.println("usage: stero.wav x y z out.wav");
+			System.err.println("usage: stero.wav volume");
 			return;
 		}
 
 		String w1_filename = args[0];
-		String lr = args[1];
-		String ud = args[3];
-		String fb = args[2];
-		//String w2_filename = args[1];
-		//String ds_out = args[2];
-		String fft_out = args[4];
+		String vol = args[1];
 
-		double leftright = 0;
-		double updown = 0;
-		double frontback = 0;		
+		double volume = 0;
 
 		try{
-    			leftright = Double.valueOf(lr).doubleValue();
-	                updown = Double.valueOf(ud).doubleValue();
-        	        frontback = Double.valueOf(fb).doubleValue();
+    			volume = Double.valueOf(vol).doubleValue();
 			
-			if(leftright>1 || leftright<-1){
-                 	       System.err.println("LeftRight value must be between -1.0 and 1.0");
-                        	return;
-                	}
-                	if(updown>1 || updown<-1){
-                	        System.err.println("UpDown value must be between -1.0 and 1.0");
-                        	return;
-                	}
-                	if(frontback>1 || frontback<-1){
-               	        	System.err.println("FrontBack value must be between -1.0 and 1.0");
+                	if(volume<=0){
+               	        	System.err.println("Volume must be greater than zero.");
                         	return;
                 	}
 		}
@@ -53,56 +36,14 @@ class Create_Customization_Files {
 			System.err.println("must use a .wav file!");
 			return;
 		}
-		if(leftright>1 || leftright<-1){
-			System.err.println("LeftRight value must be between -1.0 and 1.0");
-			return;
-		}
-		if(updown>1 || updown<-1){
-                        System.err.println("UpDown value must be between -1.0 and 1.0");
-                        return;
-                }
-		if(frontback>1 || frontback<-1){
-                        System.err.println("FrontBack value must be between -1.0 and 1.0");
-                        return;
-                }
-		/*
-		if (! (w2_filename.toLowerCase().endsWith(".wav"))) {
-                        System.err.println("must use a .wav file!");
-                        return;
-                }
-		if (! (ds_out.toLowerCase().endsWith(".wav"))) {
-                        System.err.println("must use a .wav file!");
-                        return;
-                }
-		*/
-		if (! (fft_out.toLowerCase().endsWith(".wav"))) {
-                        System.err.println("must use a .wav file!");
-                        return;
-                }
-			
 
 		Signal sound = new Signal(w1_filename);
 		sound.display();
-		//Signal hrtf = new Signal(w2_filename);
 
-		//Convolution Direct Sum
 		long start;	
 
-		/*
-		System.out.println("Convolution Sum Process");
-                start = System.currentTimeMillis();	
-
-		Signal convolveSum = new Signal(Convolution.convolveSum(w1_filename, w2_filename), sound.sampleRate);		
-		convolveSum.display();
-		convolveSum.writeWav(ds_out);	
-
-		end = System.currentTimeMillis();
-                total = end -start;
-
-                System.out.println("Convolution Sum took "+total+" milliseconds.");
-		*/
 		//Convolution FFT
-		System.out.println("FFT Process");
+		System.out.println("Convolving Sounds...");
                 start = System.currentTimeMillis();
 
 		String[] subjects = {"003", "010", "018", "020", "021", "027", "028", "033", "040", "044"};
@@ -123,16 +64,21 @@ class Create_Customization_Files {
 						else output = output+"_3.wav";
 					}
 
-					Signal convolveFFT = new Signal(Convolution.convolveFFT(j, p, 0, sound.buffer[0], subjects[i], sound.sampleRate), sound.sampleRate);	
+					Signal convolveFFT = new Signal(Convolution.convolveFFT((double)j, (double)p, 0.0, sound.buffer[0], subjects[i]), sound.sampleRate);	
+					//Lessen the volume
+					for(int k=0; k<convolveFFT.buffer[0].length; k++){
+						convolveFFT.buffer[0][k] *= volume;
+						convolveFFT.buffer[1][k] *= volume;
+					}
 					convolveFFT.writeWav(output);
 
 					long end = System.currentTimeMillis();
                 			long total = end -start;
 					start = end;
-                			System.out.println("FFT took "+total+" milliseconds.");
+                			System.out.println("An individual FFT took "+total+" milliseconds.");
 				}
 			}
-			System.out.println("#Left = "+((subjects.length-(i+1))*4));
+			System.out.println("Convolutions Left = "+((subjects.length-(i+1))*4));
 		}
 		System.out.println("Finished!");
 		System.exit(0);
